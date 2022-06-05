75d15d04c0d06c94598b0c22677836d8ebfe4cd3
henry <abdul.h.malik@me.com>
2022-06-05 15:27:36 +1000

    added tests

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import ListView
 from .models import post
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 
 class HomePageView(ListView):
     model = post
     template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
